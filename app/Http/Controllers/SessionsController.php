4955401070212efe799fccc64dd8f2d516a5eff6
vagrant <vagrant@homestead>
2020-05-11 05:20:57 +0000

    訪問策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,6 +8,12 @@ use Auth;
 class SessionsController extends Controller
 {
     //
+    public function __construct(){
+      $this->middleware('guest',[
+        'only' => ['create']
+      ]);
+    }
+    
     public function create(){
         return view('sessions.create');
     }
@@ -23,7 +29,8 @@ class SessionsController extends Controller
 
        if (Auth::attempt($credentials, $request->has('rember'))) {
            session()->flash('success', '歡迎回來');
-           return redirect()->route('users.show', [Auth::user()]);
+           $fallback =route('users.show', Auth::user());
+           return redirect()->intended($fallback);
        } else {
            session()->flash('danger', '很抱歉，您的email與密碼錯誤');
            return redirect()->back()->withInput();
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -21,10 +21,12 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
+    public function boot(){
         $this->registerPolicies();
 
+        Gate::guessPolicyNamesUsing(function($modelClass){
+            return 'App\Policies\\'.class_basename($modelClass).'Policy';
+        });
         //
     }
 }
