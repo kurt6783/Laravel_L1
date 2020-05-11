38f77a7df70aeac97707b813dd594606e33ab6cb
kurt6783 <k0911245920@gmail.com>
2020-05-11 11:18:20 +0000

    關注按鈕

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,9 +6,15 @@
   <div class="offset-md-2 col-md-8">
     <div class="col-md-12">
       <div class="offset-md-2 col-md-8">
+
         <section class="user_info">
           @include('shared._user_info', ['user' => $user])
         </section>
+
+        @if (Auth::check())
+	      @include('users._follow_form')
+	    @endif
+
         <section class="status mt-2">
 	      @include('shared._status', ['user' => $user])
 	    </section>
