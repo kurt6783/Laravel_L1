92c1baa9e8d72a37ebfd810cd7d6f6f00295a24c
vagrant <vagrant@homestead>
2020-05-11 03:38:48 +0000

    登錄成功後導航邏輯

---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.default')
+@section('title', '登錄')
+
+@section('content')
+<div class="offset-md-2 col-md-8">
+  <div class="card ">
+    <div class="card-header">
+      <h5>登錄</h5>
+    </div>
+    <div class="card-body">
+      @include('shared._errors')
+
+      <form method="POST" action="{{ route('login') }}">
+          {{ csrf_field() }}
+
+          <div class="form-group">
+            <label for="email">email:</label>
+            <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="password">密碼:</label>
+            <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+          </div>
+
+          <button type="submit" class="btn btn-primary">登錄</button>
+      </form>
+
+      <hr>
+
+      <p>沒有帳號?<a href="{{ route('signup') }}">現在註冊!</a></p>
+    </div>
+  </div>
+</div>
+@stop
